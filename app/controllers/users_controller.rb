9a3648303e100933c5c9d943598c59ff16fdcba4
imamu0530 <imamura@eswell.co.jp>
2016-04-23 02:00:08 +0000

    kadai

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+before_action :set_user, only: [:edit, :update]
+before_action :check_user,only: [:edit, :update]
 
   def show # 追加
    @user = User.find(params[:id])
@@ -18,10 +20,31 @@ class UsersController < ApplicationController
     end
   end
 
-  private
+  def edit
+  end
 
+  def update
+    if @user.update(user_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      flash[:success] = "プロフィールを登録しました"
+      redirect_to @user
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+
+  private
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation,:profile,:area)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def check_user
+    current_user == @user
   end
 end
