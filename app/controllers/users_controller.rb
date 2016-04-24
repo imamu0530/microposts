a77373aed1181870b1cbc727626f52554543ebee
imamu0530 <imamura@eswell.co.jp>
2016-04-24 16:45:21 +0000

    kadai_shusei

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-before_action :set_user, only: [:edit, :update]
+before_action :set_user, only: [:edit,:update]
 before_action :check_user,only: [:edit, :update]
 
   def show # 追加
@@ -21,6 +21,7 @@ before_action :check_user,only: [:edit, :update]
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
